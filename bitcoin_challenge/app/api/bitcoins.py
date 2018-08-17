059877b13189337dd66076f955b5210fd8860432
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-08-17 00:15:50 -0400

    complete api specification for two days

--- bitcoin_challenge/app/api/bitcoins.py
+++ bitcoin_challenge/app/api/bitcoins.py
@@ -1,8 +1,12 @@
+import sys
 import requests
 import json
 from datetime import datetime
 from flask import jsonify, current_app, request
 from app.api import api
+
+max_price = -sys.maxsize - 1
+min_price = sys.maxsize
 
 
 @api.route('/bitcoins')
@@ -13,14 +17,56 @@ def get_bitcoin_data():
     data = requests.get(url).json()
 
     date_string_format = "%Y-%m-%dT%H:%M:%S.%fZ"
-    bitcoins = {
-        'Day': 1,
+    bitcoin1 = {
+        'day': 1,
         'price': data[1]["lastPrice"],
-        'priceChange': "up",
+        'change': "na" if 1 == 1 else round(data[2]["lastPrice"] - data[1]["lastPrice"], 2),
+        'priceChange': "na" if 1 == 1 else price_change(data[1]["lastPrice"],
+                                                        data[2]["lastPrice"]),
         'dayOfWeek': datetime.strptime(data[1]["timestamp"],
                                        date_string_format).strftime('%A'),
-        'highSinceStart': "true",
-        'lowSinceStart': "true",
+        'highSinceStart': is_high_since_start(data[1]["lastPrice"]),
+        'lowSinceStart': is_low_since_start(data[1]["lastPrice"])
     }
+    print(max_price)
+    print(min_price)
+    bitcoin2 = {
+        'day': 2,
+        'price': data[2]["lastPrice"],
+        'priceChange': "na" if 2 == 1 else price_change(data[1]["lastPrice"],
+                                                        data[2]["lastPrice"]),
+        'change': "na" if 2 == 1 else round(data[2]["lastPrice"] - data[1]["lastPrice"], 2),
+        'dayOfWeek': datetime.strptime(data[2]["timestamp"],
+                                       date_string_format).strftime('%A'),
+        'highSinceStart': is_high_since_start(data[2]["lastPrice"]),
+        'lowSinceStart': is_low_since_start(data[2]["lastPrice"])
+    }
+    bitcoins = [bitcoin1, bitcoin2]
 
     return jsonify(bitcoins), 200
+
+
+def price_change(prev, curr):
+    if curr > prev:
+        priceChange = "up"
+    elif curr < prev:
+        priceChange = "down"
+    else:
+        priceChange = "same"
+    return priceChange
+
+
+def is_high_since_start(price):
+    global max_price
+    if price > max_price:
+        max_price = price
+        return True
+    return False
+
+
+def is_low_since_start(price):
+    global min_price
+    if price < min_price:
+        min_price = price
+        return True
+    return False
